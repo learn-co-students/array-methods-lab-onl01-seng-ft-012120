607ec01a8c5d04f905ed14fcc3cac8f4a2bada56
Khalid T <khalid.tissire@gmail.com>
2020-01-23 08:32:32 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,11 +11,11 @@ def using_reverse(array)
 end
 
 def using_first(array)
-  array[0]
+  array.first
 end
 
 def using_last(array)
-  array[array.length-1]
+  array.last
 end
 
 def using_size(array)
